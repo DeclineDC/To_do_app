6241c4219e1742c1e59a4922bea0bd5e31163111
Bagel Sandwich <Calvindc96@gmail.com>
2023-01-18 17:23:48 +0100

    Added Task Entity

---
+++ app/src/main/java/com/example/to_doapp/app_features/domain/model/Task.kt
@@ -0,0 +1,17 @@
+package com.example.to_doapp.app_features.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Task(
+    val title: String,
+    val description: String,
+    val dayOfMonth: Int,
+    val month: Int,
+    val year: Int,
+    val importanceOfTask: Int,
+    val isTaskRepeatable: Boolean,
+    val isTaskNotifying: Boolean,
+    @PrimaryKey val id: Int? = null
+)

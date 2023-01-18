444fa64135587267fd59e06196118224900006f3
Bagel Sandwich <Calvindc96@gmail.com>
2023-01-18 19:19:02 +0100

    Added Event classes

---
+++ app/src/main/java/com/example/to_doapp/app_features/presentation/add_edit_task_screen/AddEditTaskEvent.kt
@@ -0,0 +1,4 @@
+package com.example.to_doapp.app_features.presentation.add_edit_task_screen
+
+class AddEditTaskEvent {
+}
---
+++ app/src/main/java/com/example/to_doapp/app_features/presentation/task_overview_screen/TaskOverviewEvent.kt
@@ -0,0 +1,6 @@
+package com.example.to_doapp.app_features.presentation.task_overview_screen
+
+sealed class TaskOverviewEvent {
+
+    object OnAddTask: TaskOverviewEvent()
+}
